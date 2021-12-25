0eaf89929c4a50b737a5fb8f098f3cc98e2ef312
Trganda <trganda>
2021-12-25 09:45:14 +0800

    create a File object with bean constructor

--- spring/learning/ch0-hello-spring/ch1-ioc-set/src/test/java/org/example/PersonType1Test.java
+++ spring/learning/ch0-hello-spring/ch1-ioc-set/src/test/java/org/example/PersonType1Test.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.File;
+
 public class PersonType1Test {
     @Test
     public void testCase01() {
@@ -13,5 +15,14 @@ public class PersonType1Test {
 
         Person p = (Person) ctx.getBean("person");
         System.out.println(p);
+    }
+
+    @Test
+    public void testCase02() {
+        String config = "type1/config.xml";
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(config);
+
+        File file = (File) ctx.getBean("ifile");
+        System.out.println(file.getName());
     }
 }
