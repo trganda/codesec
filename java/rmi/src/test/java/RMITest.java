92ed5e1085cfdc5674d944f00c7069a2a6c1117a
trganda <jongsx1203@gmail.com>
2022-09-07 17:11:03 +0800

    update

--- java/rmi/src/test/java/RMITest.java
+++ java/rmi/src/test/java/RMITest.java
@@ -1,8 +1,15 @@
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class RMITest {
     @Test
-    public void RemoteServiceTest() {
-
+    public void RemoteServiceTest() throws IOException {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("1.txt"));
+        oos.writeLong(123);
+        oos.close();
     }
 }
