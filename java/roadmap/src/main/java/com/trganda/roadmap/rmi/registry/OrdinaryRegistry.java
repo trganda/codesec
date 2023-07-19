2d8c1df8793689184c6a686e6caa0c98694e6fdb
trganda <jongsx1203@gmail.com>
2023-07-19 17:58:42 +0800

    feat: split registry and server

--- java/roadmap/src/main/java/com/trganda/roadmap/rmi/registry/OrdinaryRegistry.java
+++ java/roadmap/src/main/java/com/trganda/roadmap/rmi/registry/OrdinaryRegistry.java
@@ -2,24 +2,25 @@ package com.trganda.roadmap.rmi.registry;
 
 import com.trganda.roadmap.rmi.impl.ServerSocketFactoryImpl;
 
-import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class OrdinaryRegistry {
-    public static void main(String[] args) throws IOException {
-        int port = Integer.parseInt(args[0]);
-        String addr = args[1];
+    public static void main(String[] args) {
+        try {
+            int port = Integer.parseInt(args[0]);
+            String addr = args[1];
 
-        // create a registry and listening
-        Registry reg =
-                LocateRegistry.createRegistry(
-                        port, null, new ServerSocketFactoryImpl(InetAddress.getByName(addr)));
+            // create a registry and listening
+            Registry reg =
+                    LocateRegistry.createRegistry(
+                            port, null, new ServerSocketFactoryImpl(InetAddress.getByName(addr)));
 
-        // prevent stop current thread.
-        System.in.read();
+            // prevent stop current thread.
+            System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- java/roadmap/src/main/java/com/trganda/roadmap/rmi/server/RemoteServer.java
+++ java/roadmap/src/main/java/com/trganda/roadmap/rmi/server/RemoteServer.java
@@ -4,26 +4,30 @@ import com.trganda.roadmap.rmi.impl.RemoteServiceImpl;
 import com.trganda.roadmap.rmi.impl.ServerSocketFactoryImpl;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class RemoteServer {
-    public static void main(String[] args) throws RemoteException, UnknownHostException {
-        int registryPort = Integer.parseInt(args[0]);
-        String registryIP = args[1];
-        int serverPort = Integer.parseInt(args[2]);
-        String serverIP = args[3];
-        String name = args[4];
+    public static void main(String[] args) {
+        try {
+            int registryPort = Integer.parseInt(args[0]);
+            String registryIP = args[1];
+            int serverPort = Integer.parseInt(args[2]);
+            String serverIP = args[3];
+            String name = args[4];
 
-        RemoteServiceImpl rsi =
-                new RemoteServiceImpl(
-                        serverPort,
-                        null,
-                        new ServerSocketFactoryImpl(InetAddress.getByName(serverIP)));
-        Registry reg = LocateRegistry.getRegistry(registryIP, registryPort);
-        reg.rebind(name, rsi);
-        System.out.println("rebind");
+            System.out.println(registryPort + " " + registryIP + " " + serverPort + " " + serverIP + " " + name);
+
+            RemoteServiceImpl rsi =
+                    new RemoteServiceImpl(
+                            serverPort,
+                            null,
+                            new ServerSocketFactoryImpl(InetAddress.getByName(serverIP)));
+            Registry reg = LocateRegistry.getRegistry(registryIP, registryPort);
+            reg.rebind(name, rsi);
+            System.out.println("rebind");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
